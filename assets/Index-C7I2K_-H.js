8181176cdd3c77f6933c3b54fe8ed5f7c55b884f
cubfe <kakusoft@163.com>
2025-01-16 23:23:20 +0800

    update dist

---
+++ assets/Index-C7I2K_-H.js
@@ -0,0 +1,1 @@
+import{d as n,V as s,a,b as u}from"./index-C5p2ZnNO.js";const _=n({__name:"Index",setup(c){const e=a(),r=u();return s(()=>{const{path:t}=e.params,o=e.query;r.replace({path:"/"+t,query:o})}),(t,o)=>null}});export{_ as default};
